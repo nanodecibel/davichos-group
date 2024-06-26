29f52d10de6d08a48200d82dccc9e76ec2bbdb2c
jallauca@udlanet.ec <jallauca@udlanet.ec>
2024-06-26 09:33:20 -0500

    primera publicacion dominio noCname

--- dist/davichos-group/server/main.server.mjs
+++ dist/davichos-group/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-O7TSYC6M.mjs";import"./chunk-QZOVVAVC.mjs";import"./chunk-NDYDZJSS.mjs";export{a as default};
+import{a}from"./chunk-DOVGOHHB.mjs";import"./chunk-QZOVVAVC.mjs";import"./chunk-NDYDZJSS.mjs";export{a as default};
